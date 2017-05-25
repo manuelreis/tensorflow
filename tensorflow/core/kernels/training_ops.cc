b0b05a94fad5ce88fadac2a1bb586ab7151d0959
Manuel Reis <manuel.barreto.reis@gmail.com>
2017-05-25 16:13:16 +0100

    Revert "Gradient Descent Op using HTM"
    This reverts commit ccc6d74da0983c3143a6d7118c0d2b86203eea24.

--- tensorflow/core/kernels/training_ops.cc
+++ tensorflow/core/kernels/training_ops.cc
@@ -22,8 +22,6 @@ limitations under the License.
 #include "tensorflow/core/kernels/bounds_check.h"
 #include "tensorflow/core/kernels/variable_ops.h"
 
-#include "tm.h"
-
 namespace tensorflow {
 
 using CPUDevice = Eigen::ThreadPoolDevice;
@@ -379,7 +377,7 @@ class ApplyGradientDescentOp : public OpKernel {
   }
 
   void Compute(OpKernelContext* ctx) override {
-    //auto locks = MaybeLockMutexesInOrder(ctx, use_exclusive_lock_, {0});
+    auto locks = MaybeLockMutexesInOrder(ctx, use_exclusive_lock_, {0});
     Tensor var;
     OP_REQUIRES_OK(ctx, GetInputTensor(ctx, 0, use_exclusive_lock_, &var));
 
@@ -399,15 +397,9 @@ class ApplyGradientDescentOp : public OpKernel {
                                 delta.shape().DebugString()));
 
     const Device& device = ctx->template eigen_device<Device>();
-    
-    htm_budget = HTM_RETRIES;
-   
-    mutex *mutex = GetMutex(ctx, 0);
-
-    TM_BEGIN(mutex);
     functor::ApplyGradientDescent<Device, T>()(
         device, var.flat<T>(), alpha.scalar<T>(), delta.flat<T>());
-    TM_END(mutex);
+
     MaybeForwardRefInputToRefOutput(ctx, 0, 0);
   }
 
