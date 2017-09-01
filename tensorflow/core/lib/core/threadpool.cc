4c8c32520d355faa93c5140dd991081269f02213
Davide Leoni <davide.leoni90@gmail.com>
2017-09-01 18:33:56 +0100

    Add pinning

--- tensorflow/core/lib/core/threadpool.cc
+++ tensorflow/core/lib/core/threadpool.cc
@@ -24,6 +24,12 @@ limitations under the License.
 #include "tensorflow/core/platform/setround.h"
 #include "tensorflow/core/platform/tracing.h"
 #include "tensorflow/core/platform/types.h"
+// (dleoni) Include header for the thread pinning
+#include <sched.h>
+#include "tensorflow/core/platform/cpu_info.h"
+
+// (dleoni) The local thread unique identifier
+__thread unsigned local_thread_id(0);
 
 namespace tensorflow {
 namespace thread {
@@ -42,17 +48,55 @@ struct EigenEnvironment {
   Env* const env_;
   const ThreadOptions thread_options_;
   const string name_;
+  // (dleoni) Set two counters to determine the core whom the next thread
+  // for both the intra and the inter parallelism pools will be pinned to
+  // The main process is the only one that increments those variables when
+  // it creates the two pools, so we don't need any synchronization
+  unsigned intra_pool_next_core_;
+  unsigned inter_pool_next_core_;
+  
 
   EigenEnvironment(Env* env, const ThreadOptions& thread_options,
                    const string& name)
-      : env_(env), thread_options_(thread_options), name_(name) {}
+      : env_(env), thread_options_(thread_options), name_(name), intra_pool_next_core_(0), inter_pool_next_core_(std::thread::hardware_concurrency()-1) {}
 
   EnvThread* CreateThread(std::function<void()> f) {
-    return env_->StartThread(thread_options_, name_, [=]() {
+    return env_->StartThread(thread_options_, name_, [=, &local_thread_id]() {
+      unsigned core = 0;
+      unsigned num_cores = std::thread::hardware_concurrency();
       // Set the processor flag to flush denormals to zero.
       port::ScopedFlushDenormal flush;
       // Set the processor rounding mode to ROUND TO NEAREST.
       port::ScopedSetRound round(FE_TONEAREST);
+      // (dleoni) Check if the thread belongs to the intra-op or to the inter-op pool
+      if (name_ == "tf_Eigen") {
+        // (dleoni) The thread belongs to the intra-op pool => get the number of core
+        // from the corresponding variable
+        core = intra_pool_next_core_;
+        // Update the counter for the next core to be used for this pool
+        intra_pool_next_core_ = (intra_pool_next_core_ + 1) % num_cores;
+      }
+      else {
+        core = inter_pool_next_core_;
+        // Update the counter for the next core to be used for this pool
+        inter_pool_next_core_ = inter_pool_next_core_ - 1;
+        if (inter_pool_next_core_ < 0) {
+          inter_pool_next_core_ = num_cores -1;
+        }
+      }
+      // (dleoni) Pin the thread to the core indicated in the field "core"
+      int ret = 0;
+      cpu_set_t new_cpu_set;
+      CPU_ZERO(&new_cpu_set);
+      CPU_SET(core, &new_cpu_set);
+      ret = sched_setaffinity(0, sizeof(cpu_set_t), &new_cpu_set);
+      if (ret != 0) {
+        VLOG(WARNING) << "sched_setaffinity";
+      }
+      // (dleoni) Set the local thread identifier to the index of the core it
+      // is pinned to; this is used to capture statistics about the transactions
+      // performed by this thread 
+      local_thread_id = core;
       f();
     });
   }
@@ -111,7 +155,26 @@ ThreadPool::ThreadPool(Env* env, const ThreadOptions& thread_options,
 ThreadPool::ThreadPool(Env* env, const ThreadOptions& thread_options,
                        const string& name, int num_threads,
                        bool low_latency_hint) {
+  cpu_set_t new_cpu_set;
+  cpu_set_t old_cpu_set;
+  int ret = 0;
   CHECK_GE(num_threads, 1);
+  // (dleoni) Check if the main thread has already been pinned to a core: if not,
+  // pin it to the core 0
+  ret = sched_getaffinity(0, sizeof(cpu_set_t), &old_cpu_set);
+  if (ret != 0) {
+    VLOG(WARNING) << "sched_getaffinity";
+  }
+  if (!CPU_COUNT(&old_cpu_set) || CPU_COUNT(&old_cpu_set) > 1) {
+    // (dleoni) The main thread has not been pinned yet, so pin it to the current
+    // core now 
+    CPU_ZERO(&new_cpu_set);
+    CPU_SET(0, &new_cpu_set);
+    ret = sched_setaffinity(0, sizeof(cpu_set_t), &new_cpu_set);
+  }
+  if (ret != 0) {
+    VLOG(WARNING) << "sched_setaffinity";
+  }
   impl_.reset(new ThreadPool::Impl(env, thread_options, "tf_" + name,
                                    num_threads, low_latency_hint));
 }
