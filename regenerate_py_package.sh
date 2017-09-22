8765cee7c277add155cbcfaa2c6dd32e0c3208c5
Davide Leoni <davide.leoni90@gmail.com>
2017-09-22 15:28:50 +0100

    Tensorflow version for Intel, with htm and pinning of threads (NO TIME MEASURING)

--- regenerate_py_package.sh
+++ regenerate_py_package.sh
@@ -4,4 +4,9 @@ bazel build -c opt -c dbg --verbose_failures --strip=never  //tensorflow/tools/p
 rm -rf ~/tmp/tensorflow_pkg/
 bazel-bin/tensorflow/tools/pip_package/build_pip_package ~/tmp/tensorflow_pkg
 /usr/bin/yes | pip uninstall tensorflow
-pip install --user ~/tmp/tensorflow_pkg/tensorflow-1.1.0rc0-cp27-cp27mu-linux_x86_64.whl
+project_name=$(sed -n 's/^project_name = //p' ~/tensorflow/tensorflow/tools/pip_package/setup.py | tr -d "'")
+wheel_name="${HOME}/tmp/tensorflow_pkg/${project_name}-1.1.0rc0-cp27-cp27mu-linux_x86_64.whl"
+rm -rf ~/${project_name}
+mkdir ~/${project_name}
+pip install --target=${HOME}/${project_name} ${wheel_name}
+export PYTHONPATH=~/${project_name}
